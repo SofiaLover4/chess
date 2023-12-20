94d0479b29dd1a928b4ea9893bd641d75bc2c76f
Adrian Perez <adrian1804perez@gmail.com>
2023-12-20 09:04:39 -0800

    Add ChessBoard class

--- lib/board.rb
+++ lib/board.rb
@@ -14,3 +14,40 @@ class Square
     @content = format.call('   ')
   end
 end
+
+# A class for the board the user will see
+class ChessBoard
+  # ChessBoard is going to be the graph
+  attr_accessor :board
+
+  def create_board # rubocop:disable Metrics/MethodLength
+    # Creating the board with colored squares
+    board = Array.new(8) { [] }
+
+    i = 0
+    while i < 8
+      j = 0
+      while j < 8
+        square = (i + j).even? ? '   '.on_white : '   '.on_black
+        board[i].push(square)
+        j += 1
+      end
+      i += 1
+    end
+    board
+  end
+
+  def initialize
+    # Class starts with an empty board
+    @board = create_board
+  end
+
+  def show_board
+    @board.each do |column|
+      column.each do |tile|
+        print tile
+      end
+      print "\n"
+    end
+  end
+end
